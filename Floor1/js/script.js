8b8167d6ec102f8a2f81f5cfcb0cd685f25d5d85
Niagaolang <Niagaolang@gmail.com>
2026-09-02 14:44:01 +0700

    Add files via upload

---
+++ Floor1/js/script.js
@@ -0,0 +1,29 @@
+const DEBUG = false;
+
+function debugLog(...args) {
+    if (DEBUG) {
+        console.log(...args);
+    }
+}
+
+fetch("/Header&Footer/header.html")
+    .then(response => response.text())
+    .then(data => {
+        document.querySelector("#header").innerHTML = data;
+    });
+
+
+fetch("/Header&Footer/footer.html")
+    .then(response => response.text())
+    .then(data => {
+        document.querySelector("#footer").innerHTML = data;
+    });
+
+// Toggle Mobile Menu
+function toggleMenu() {
+    const navMenu = document.getElementById("navMenu");
+
+    if (navMenu) {
+        navMenu.classList.toggle("active");
+    }
+}
